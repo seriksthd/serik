56e9f5a89cc1bde50299aa14c718a76059ca70e3
serik <s29014517@gmail.com>
2025-07-11 18:00:20 +0600

    Update link.js

--- src/utils/constants/link.js
+++ src/utils/constants/link.js
@@ -20,15 +20,8 @@ export const Link = [
       "https://img.utdstc.com/icon/32d/af5/32daf5ad34af15eb8ea853325ec1838574e3a6d572c204950d42471cc2ceba94:200",
   },
   {
-    id: 4,
-    url: "https://t.me/+0vsINRgPyQs4ODky",
-    title: "telegram grup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/1200px-Telegram_2019_Logo.svg.png",
-  },
-  {
     id: 5,
-    url: "https://t.me/Ser1k08",
+    url: "https://t.me/+aykeI70Z8HRjYzYy",
     title: "telegram",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/1200px-Telegram_2019_Logo.svg.png",
